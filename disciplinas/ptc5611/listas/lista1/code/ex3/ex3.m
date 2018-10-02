7aa7de9008d29b4f2e97e91ea519ea32d648e35e
Bruno Peixoto <brunolnetto@gmail.com>
2018-10-02 11:16:05 -0300

    Imagens do exercicio 3

--- disciplinas/ptc5611/listas/lista1/code/ex3/ex3.m
+++ disciplinas/ptc5611/listas/lista1/code/ex3/ex3.m
@@ -1,23 +1,32 @@
-clear all; close all; clc
+clear all; 
+close all; 
+clc;
+
+% [s]
 Ts = 0.1;
 
-sim('ex3sim');
+% [rad/s]
+ws = 2*pi/Ts;
 
-hfig = figure();
-set(0, 'defaulttextinterpreter', 'latex');
+% Funcoes de transferencia 
+G = tf(1, [1,0]);
+Ga = -0.01*tf(1, [1, -1.001], Ts, 'Variable', 'z^-1');
+Gb = -0.005*tf([1 -1], [1 -1.001], Ts, 'Variable', 'z^-1');
 
-plot(x.time, x.signals.values, 'k');
+hfig = figure('units','normalized','outerposition',[0 0 1 1]);
+set(hfig, 'defaultLegendInterpreter','latex');
+
+bode(G);
 hold on;
-plot(y.time, y.signals.values);
+bode(Ga);
 hold on;
-plot(y_red.time, y_red.signals.values, 'o');
+bode(Gb);
 hold off;
-legend('Entrada', 'Diagrama fornecido', 'Diagrama reduzido');
-title('Compara\c{c}\~ao do diagrama fornecido e redu\c{c}\~ao de fun\c{c}\~ao de transfer\^encia');
-xlabel('Tempo [s]');
-ylabel('Amplitude');
 
-axis([0, 1, -3, 3]);
+legend('$\frac{1}{s}$', 'Estritamente própria', 'Biprópria', 'fontsize', '20');
+title('Diagrama de bode de 1/s', 'interpreter', 'latex');
+xlabel('Frequência de entrada', 'interpreter', 'latex');
 
-saveas(hfig, '../../report/images/ex31.png');
-saveas(hfig, '../../report/images/ex31.eps');
+xlim([1, 1.5*ws/2]);
+saveas(hfig, '../../report/images/ex3.png');
+saveas(hfig, '../../report/images/ex3.eps');
