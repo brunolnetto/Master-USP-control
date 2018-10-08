b20b86b1ffccc8ca8e888fe01c087c9dd9fefae1
Bruno Peixoto <brunolnetto@gmail.com>
2018-10-08 13:55:59 -0300

    WIP ex6 simulink incomplete

--- disciplinas/ptc5611/listas/lista1/report/code/runsim.m
+++ disciplinas/ptc5611/listas/lista1/report/code/runsim.m
@@ -1,20 +1,24 @@
-function [] = runsim(Cs, Ts, simmodel_fname)
-    global u_ant; global e_ant;
-    
-    assignin('base', 'Ts', Ts)
-    
+function [ans] = runsim(Cs, Ts, simname)
     Cz = c2d(Cs, Ts, 'tustin');
-    Cz.variable  = 'z^-1';
 
-    [m, n] = size(Cz);
-
-    u_ant = zeros(m, 1);
-    e_ant = zeros(n, 1);
-
+    Cz.Variable  = 'z^-1';
+    
     Cnum = Cz.num{:};
     Cden = Cz.den{:}; 
     
-    assignin('base', 'Cnum', Cnum);
-    assignin('base', 'Cden', Cden);
+    Cnum = Cnum';
+    Cden = Cden';
     
-    sim(simmodel_fname);
+    m = length(Cnum);
+    n = length(Cden);
+    
+    % Erro e entradas iniciais
+    u0 = zeros(m-1, 1);
+    e0 = zeros(n, 1);
+
+    % Tempo de simulacao [s]
+    Tf = 10;
+    
+    stOutput = sim(simname, Tf);
+    
+    ans = stOutput;
