a8339e55ff37780a851436ca61913e730635c1d5
brunolnetto <brunolnetto@gmail.com>
2018-10-18 00:18:16 -0300

    imagens ex6 e alteracao do script 6

--- disciplinas/ptc5611/listas/lista1/report/code/ex6/ex6.m
+++ disciplinas/ptc5611/listas/lista1/report/code/ex6/ex6.m
@@ -24,9 +24,9 @@ M = 0.2;
 zeta = sqrt(log(M)^2/(log(M)^2 + pi^2));
 wn = (1/tr)*(1/sqrt(1 - zeta^2))*(pi - atan(sqrt(1 - zeta^2)/zeta));
 polo_proj = wn*(-zeta + i*sqrt(1 - zeta^2));
-% ----------------------------------------------
+% 
 
-% ------------- Lugar das raizes ---------------
+% Lugar das raizes
 %         ^\omega 
 %     |  |          -: Polos em malha fechada
 %     -  |          x: Polos do sistema 
@@ -38,7 +38,6 @@ polo_proj = wn*(-zeta + i*sqrt(1 - zeta^2));
 
 C = (wn^2)*(s + c)/(s + 2*zeta*wn);
 % ----------------------------------------------
-
 
 
 % ------------- Simulacoes ---------------------
@@ -66,7 +65,6 @@ Cz20 = c2d(C, Ts, 'tustin');
 % ##############################################    
 
 % ############### ZOH ~ Pade' ##################
-% ##############################################
 
 % Frequencia = 4 Hz
 fs = 4;
@@ -86,12 +84,15 @@ C20_pade = C_zoh_approx_pade(G, polo_proj, Ts);
 Cz20_pade = c2d(C20_pade, Ts, 'tustin');
 [e20pade, u20pade, y20pade] = sim_ex6(G, Cz20_pade, 'ex6discreto', Tf);
 
+% ##############################################
+% ----------------------------------------------
+
 % ------------- Visualizacao -------------------
 
 % Plotagem dos sinais adquiridos
 legends = {'Continuo', ...
-           'f_s = 4 Hz', 'f_s = 20 Hz', ...
-           'f_s = 4 Hz com Pade', 'f_s = 20 Hz com Pade'};
+           '$f_s$ =  4 Hz', '$f_s$ = 20 Hz', ...
+           '$f_s$ = 4 Hz com Pade', '$f_s$ = 20 Hz com Pade'};
 
 E = [ec, e4, e20, e4pade, e20pade];
 U = [uc, u4, u20, u4pade, u20pade];
@@ -99,16 +100,18 @@ Y = [yc, y4, y20, y4pade, y20pade];
 
 % Plota a a��o de controle e sa�das
 hfig = plot_ex6(E, U, Y, legends);
-saveas(gca, '../../report/images/ex6zoh.pdf');
+saveas(hfig(1), '../../images/errozoh.pdf');
+saveas(hfig(2), '../../images/saida6zoh.pdf');
+saveas(hfig(3), '../../images/controlezoh.pdf');
 %close(hfig);
 
 % Salva modelos em simulink
 open_system('ex6continuo')
-print('-sex6continuo','-dpdf','../../report/images/ex6simcontinuo.pdf')
+print('-sex6continuo','-dpdf','../../images/ex6simcontinuo.pdf')
 close_system
 
 open_system('ex6discreto')
-print('-sex6discreto','-dpdf','../../report/images/ex6simdiscreto.pdf')
+print('-sex6discreto','-dpdf','../../images/ex6simdiscreto.pdf')
 close_system
 
 % ----------------------------------------------
