649d495b3c9b953a793c83925ee2b988cd666b86
brunolnetto <brunolnetto@gmail.com>
2018-12-06 01:06:17 -0300

    Calcula trajetoria circular e Efetua o calculo inverso das coordenadas redundantes

--- disciplinas/multicorpos/projeto/monografia/code/main.m
+++ disciplinas/multicorpos/projeto/monografia/code/main.m
@@ -12,3 +12,13 @@ mechanism.eqdyn = orsino_eqdyn(mechanism);
 
 toc(t0)
 
+traj = trajectory();
+
+t0 = tic;
+[~, n] = size(traj.q);
+obj_vals = [];
+for i = 1:n
+    [q_circ_, obj_val] = q_circ(mechanism, zeros(1, 6), traj.q(:, i).')
+    obj_vals = [obj_vals; obj_val];
+end
+toc(t0)
