057671689d3523c375372024eea12305d72c410f
Bruno Peixoto <brunolnetto@gmail.com>
2018-12-14 16:40:54 -0200

    Plotagem gráficos de ação de controle e ângulos

--- disciplinas/multicorpos/projeto/monografia/code/plot_qpu.m
+++ disciplinas/multicorpos/projeto/monografia/code/plot_qpu.m
@@ -50,42 +50,71 @@ function plot_qpu(sims)
     figure();
     subplot(3, 1, 1);
     plot(t, u(:,1));
+    title('Torque $\tau_1$', 'interpreter', 'latex')
+    
     subplot(3, 1, 2);
     plot(t, u(:,2));
+    title('Torque $\tau_2$', 'interpreter', 'latex')    
+    
     subplot(3, 1, 3);
     plot(t, u(:,3));
+    title('Torque $\tau_3$', 'interpreter', 'latex')    
     
     figure();
     subplot(3, 2, 1);
     plot(t, q(:,1));
+    title('${}^1 q_1$', 'interpreter', 'latex');
+    
     subplot(3, 2, 2);
     plot(t, q(:,2));
+    title('${}^1 q_2$', 'interpreter', 'latex');
+    
     subplot(3, 2, 3);
     plot(t, q(:,3));
+    title('${}^2 q_1$', 'interpreter', 'latex');
+    
     subplot(3, 2, 4);
     plot(t, q(:,1));
+    title('${}^2 q_2$', 'interpreter', 'latex');
+    
     subplot(3, 2, 5);
     plot(t, q(:,2));
+    title('${}^3 q_1$', 'interpreter', 'latex');
+    
     subplot(3, 2, 6);
     plot(t, q(:,3));
+    title('${}^3 q_2$', 'interpreter', 'latex');
     
     figure();
     subplot(3, 2, 1);
     plot(t, p(:,1));
+    title('${}^1 p_1$', 'interpreter', 'latex');
+    
     subplot(3, 2, 2);
     plot(t, p(:,2));
+    title('${}^1 p_2$', 'interpreter', 'latex');
+    
     subplot(3, 2, 3);
     plot(t, p(:,3));
+    title('${}^2 p_1$', 'interpreter', 'latex');
+    
     subplot(3, 2, 4);
     plot(t, p(:,1));
+    title('${}^2 p_2$', 'interpreter', 'latex');
+    
     subplot(3, 2, 5);
     plot(t, p(:,2));
+    title('${}^3 p_1$', 'interpreter', 'latex');
+    
     subplot(3, 2, 6);
     plot(t, p(:,3));
+    title('${}^3 p_2$', 'interpreter', 'latex');
     
     figure();
     plot(t, constraints_error);
+    title('$\varphi(\mathbf{q}) = \epsilon_\varphi$', 'interpreter', 'latex');
 
     figure();
     plot(t, q_error);
+    title('$|\mathbf{q}_{k} - \mathbf{q}_{k-1}| = \epsilon_{\Delta q}$', 'interpreter', 'latex');
 end
--- disciplinas/multicorpos/projeto/monografia/code/trajectory.m
+++ disciplinas/multicorpos/projeto/monografia/code/trajectory.m
@@ -1,19 +1,18 @@
-function  traj = trajectory()
-    % [s]
-    dt = 0.01;
-    
+function  traj = trajectory()   
     % [m]
-    R = 0.2;
+    R = 0.15;
     
     % [rad/s]
     omega = 10;
     
     % End time [s]
-    tf = 2*pi/omega;
+    tf = 5;
     
     % Time vector
     n = 70;
     t = linspace(0, tf, n);
+    
+    dt = t(2) - t(1);
     
     q = [R*cos(omega*t); R*sin(omega*t); omega*t].';
     qp = [-R*omega*sin(omega*t); R*omega*cos(omega*t); omega*ones(size(t))].';
