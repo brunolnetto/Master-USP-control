530814c203d0d4908fb27c7e1031ad782816efa8
Bruno Peixoto <brunolnetto@gmail.com>
2019-06-17 17:20:51 -0300

    Modelo - Uniciclo

---
+++ disciplinas/controle_nlinear/main.m
@@ -0,0 +1,8 @@
+% Author: Bruno Peixoto
+% Date: 17/06/19
+
+addpath('/home/brunopeixoto/github/Robotics4fun/')
+addpath(genpath('/home/brunopeixoto/github/Robotics4fun/'))
+savepath
+
+turtlebot = load_turtlebot();
